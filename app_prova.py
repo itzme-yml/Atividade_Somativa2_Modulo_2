25c02735e9e54c2e1c9df3e357d915f280ce922b
Yasmin <lopesyasmin.yuri@gmail.com>
2026-08-25 14:53:50 -0300

    Add files via upload

---
+++ app_prova.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+# Página inicial explicando como usar
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/soma")
+def soma():
+    v1 = float(request.args.get("valor1", 0))
+    v2 = float(request.args.get("valor2", 0))
+    return {"resultado": v1 + v2 }
+
+## Rota subtrair
+
+@app.route("/subtrair")
+def subtrair():
+    v1 = float(request.args.get("valor1", 0))
+    v2 = float(request.args.get("valor2", 0))
+    return {"resultado": v1 - v2}
+
+
+## Rota Multiplicar
+
+@app.route("/multiplicar")
+def multiplicar():
+    v1 = float(request.args.get("valor1", 0))
+    v2 = float(request.args.get("valor2", 0))
+    return {"resultado": v1 * v2}
+
+
+## Rota Dividir
+@app.route("/dividir")
+def dividir():
+    v1 = float(request.args.get("valor1", 0))
+    v2 = float(request.args.get("valor2", 0))
+
+    if v2 == 0:
+        return {"erro": "Não é possível dividir por zero"}
+
+    return {"resultado": v1 / v2}
+
+if __name__ == "__main__":
+    app.run(debug=True)
